1e132f9339c376387ab8f76064e6ac2b45707c0e
DevoRoberts <devorob26@gmail.com>
2018-09-15 17:52:07 -0400

    Add getters and setters for weather data model.

--- app/src/main/java/com/devodriqroberts/storms/Models/CurrentWeather.java
+++ app/src/main/java/com/devodriqroberts/storms/Models/CurrentWeather.java
@@ -1,22 +1,78 @@
 package com.devodriqroberts.storms.Models;
 
+import com.devodriqroberts.storms.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class CurrentWeather {
     private String locationLabel;
     private String icon;
     private long time;
-    private double temperature;
-    private double humidity;
-    private double precipProbability;
+    private long temperature;
+    private long humidity;
+    private long precipProbability;
     private String summary;
+    private String timeZone;
 
-    public CurrentWeather(String locationLabel, String icon, long time, double temperature, double humidity, double precipProbability, String summary) {
+    public CurrentWeather(String locationLabel, String icon, long time, double temperature, double humidity, double precipProbability, String summary, String timeZone) {
         this.locationLabel = locationLabel;
         this.icon = icon;
         this.time = time;
-        this.temperature = temperature;
-        this.humidity = humidity;
-        this.precipProbability = precipProbability;
+        this.temperature = (Math.round(temperature));
+        this.humidity = Math.round(humidity * 100);
+        this.precipProbability = (Math.round(precipProbability) * 100 );
         this.summary = summary;
+        this.timeZone = timeZone;
+
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(timeZone));
+        Date dateTime = new Date(time * 1000);
+        return formatter.format(dateTime);
+    }
+
+    public int getIconId() {
+
+        int iconId = R.drawable.clear_day;
+
+        switch (icon) {
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+        }
+
+        return iconId;
     }
 
     public String getLocationLabel() {
--- app/src/main/java/com/devodriqroberts/storms/Views/MainActivity.java
+++ app/src/main/java/com/devodriqroberts/storms/Views/MainActivity.java
@@ -9,6 +9,10 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.devodriqroberts.storms.R;
+import com.devodriqroberts.storms.Models.CurrentWeather;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String KEY = "441ab0b3be13f34c82e80dc9fe018506";
     private static final String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather currentWeather;
     private String forcastUrl;
     private double latitude;
     private double longitude;
@@ -55,18 +60,44 @@ public class MainActivity extends AppCompatActivity {
                 public void onResponse(Call call, Response response) throws IOException {
 
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
 
+                            currentWeather = getCurrentDetails(jsonData);
                         } else {
                             alertUserAboutError();
                         }
-                    } catch (IOException e) {
-                        Log.e(TAG, "IO Exception caught: ", e);
+                    } catch (IOException ioe) {
+                        Log.e(TAG, "IO Exception caught: ", ioe);
+                    } catch (JSONException jsone) {
+                        Log.e(TAG, "JSON Exception caught: ", jsone);
                     }
                 }
             });
         }
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        String timezone = forecast.getString("timezone");
+
+        String icon = currently.getString("icon");
+        String summary = currently.getString("summary");
+        double temperature = currently.getDouble("temperature");
+        double humidity = currently.getDouble("humidity");
+        double precipProbability = currently.getDouble("precipProbability");
+        long time = currently.getLong("time");
+
+
+
+        currentWeather = new CurrentWeather("Alcatraz Island, CA", icon, time, temperature, humidity, precipProbability, summary, timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+
+        return currentWeather;
     }
 
     private boolean isNetworkAvailable() {
